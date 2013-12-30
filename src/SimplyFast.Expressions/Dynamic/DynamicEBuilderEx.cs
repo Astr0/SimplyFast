87a1734ace20525eb10c02e36d4c955d4c7147e5
Andriy Obrizan <aastr00@gmail.com>
2013-12-30 11:35:51 +0200

    Dynamic Expressions.

---
+++ src/SimplyFast.Expressions/Dynamic/DynamicEBuilderEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SF.Expressions.Dynamic
+{
+    public static class DynamicEBuilderEx
+    {
+        #region Expr Builder
+
+        /// <summary>
+        /// Creates DynamicEBuilder from expression
+        /// </summary>
+        public static dynamic EBuilder(this Expression expression)
+        {
+            return DynamicEBuilder.Create(expression);
+        }
+
+        /// <summary>
+        /// Create DynamicEBuilder from type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static dynamic EBuilder(this Type type)
+        {
+            return DynamicEBuilder.Create(type);
+        }
+
+        #endregion
+    }
+}
---
+++ src/SimplyFast.Expressions/Dynamic/Internal/StaticEBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq.Expressions;
+using SF.Reflection;
+
+namespace SF.Expressions.Dynamic
+{
+    internal class StaticEBuilder : DynamicEBuilder
+    {
+        private readonly Type _type;
+
+        public StaticEBuilder(Type type)
+        {
+            _type = type;
+        }
+
+        protected override Expression Operation
+        {
+            get { return Expression.Constant(_type, typeof (Type)); }
+        }
+
+        protected override DynamicEBuilder Set(string name, Expression value)
+        {
+            return GetExpr(name).Assign(value);
+        }
+
+        protected override DynamicEBuilder Binary(ExpressionType operation, Expression value)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override DynamicEBuilder Method(string name, params Expression[] args)
+        {
+            return EBuilder.Method(_type, name, args);
+        }
+
+        protected override DynamicEBuilder GetIndex(params Expression[] index)
+        {
+            return GetIndexExpr(index);
+        }
+
+        private IndexExpression GetIndexExpr(Expression[] index)
+        {
+            return EBuilder.Index(_type, index);
+        }
+
+        protected override DynamicEBuilder SetIndex(Expression[] index, Expression value)
+        {
+            return GetIndexExpr(index).Assign(value);
+        }
+
+        protected override DynamicEBuilder Invoke(Expression[] args)
+        {
+            return EBuilder.New(_type, args);
+        }
+
+        public override DynamicEBuilder Get(string name)
+        {
+            return GetExpr(name);
+        }
+
+        private MemberExpression GetExpr(string name)
+        {
+            return EBuilder.MemberAccess(null, _type.FieldOrProperty(name));
+        }
+
+        public override DynamicEBuilder Unary(ExpressionType operation)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override DynamicEBuilder Convert(Type type)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
