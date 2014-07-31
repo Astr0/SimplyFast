00202561629df2adece2a3f0a24c9e2e74dc44a4
Andriy Obrizan <aastr00@gmail.com>
2014-07-31 14:16:46 +0300

    Added protobuf stuff.

---
+++ src/SimplyFast.Protobuf/Internal/ProtobufProducer.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using ProtoBuf;
+using SF.Pipes;
+
+namespace SF.Protobuf
+{
+    internal class ProtobufProducer<T>: IProducer<T>
+    {
+        private readonly IProducer<byte[]> _producer;
+
+        public ProtobufProducer(IProducer<byte[]> producer)
+        {
+            _producer = producer;
+        }
+
+        public Task Add(T obj, CancellationToken cancellation = new CancellationToken())
+        {
+            using (var ms = new MemoryStream())
+            {
+                Serializer.Serialize(ms, obj);
+                var buf = ms.ToArray();
+                return _producer.Add(buf, cancellation);
+            }
+        }
+
+        public void Dispose()
+        {
+            _producer.Dispose();
+        }
+    }
+}
---
+++ src/SimplyFast.Protobuf/ProtobufEx.cs
@@ -0,0 +1,17 @@
+﻿using SF.Pipes;
+
+namespace SF.Protobuf
+{
+    public static class ProtobufEx
+    {
+        public static IConsumer<T> AsProtobufConsumer<T>(this IConsumer<byte[]> consumer)
+        {
+            return new ProtobufConsumer<T>(consumer);
+        }
+
+        public static IProducer<T> AsProtobufProducer<T>(this IProducer<byte[]> producer)
+        {
+            return new ProtobufProducer<T>(producer);
+        }
+    }
+}
