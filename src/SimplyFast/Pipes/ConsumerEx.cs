97e945fbbc9d20045b40c6c05713cb91e65a2968
astr0 <aastr00@gmail.com>
2015-11-27 15:30:31 +0200

    Producer/Consumer stuff.

---
+++ src/SimplyFast/Pipes/ConsumerEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SF.Tests.Pipes;
+
+namespace SF.Pipes
+{
+    public static class ConsumerEx
+    {
+        /// <summary>
+        /// Creates consumer from event pattern. Action will block if nothing taken
+        /// </summary>
+        public static IConsumer<T> FromEvent<T>(Func<Action<T>, IDisposable> subscribe)
+        {
+            return new EventConsumer<T>(subscribe);
+        }
+    }
+}
---
+++ src/SimplyFast/Pipes/Todo.cs
@@ -0,0 +1,5 @@
+﻿// TODO: Pipes are mega cool, almost like IEnumerable
+// 1. A Lot of things to pipe
+//  - event to pipe
+// 2. Pipe to a lot of things
+//  - pipe to event invocation
