660c3d27857c2131b15e65f1edbea88532704a24
Andriy.Obrizan <andriy_obrizan@softatbest.com>
2019-02-15 13:13:13 +0200

    Moved EqualityComparerEx to SF root

--- src/SimplyFast/EqualityComparerEx.cs
+++ src/SimplyFast/EqualityComparerEx.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using SimplyFast.Comparers;
 
-namespace SimplyFast.Comparers
+namespace SimplyFast
 {
     /// <summary>
     ///     Facade for Comparers
